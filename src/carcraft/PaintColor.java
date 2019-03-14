8d68257089c475c257815005e61a6af6301e5321
CarterK33 <C_k133@txstate.edu>
2019-03-13 22:03:07 -0500

    Add files via upload

---
+++ src/carcraft/PaintColor.java
@@ -0,0 +1,20 @@
+package carcraft; 
+
+public class PaintColor extends CarOptions {
+
+public Color(ICar car)
+{
+	System.out.println("Adding paint color to vechile..")
+}
+
+@Overrride
+public String GetDescription(){
+	return super.GetDescription() + " Paint Color"
+}
+
+
+
+
+
+
+}
