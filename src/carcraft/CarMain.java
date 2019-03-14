096f9cb3f945068eaaec4bd481393a5743608faa
dal79 <dal79@users.noreply.github.com>
2019-03-13 23:30:12 -0500

    Update CarMain.java

--- src/carcraft/CarMain.java
+++ src/carcraft/CarMain.java
@@ -1,14 +1,34 @@
 package carcraft;
 
-
 public class CarMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Starting: Creating Cars using Decorator Pattern\n");
 
-		ICar carA = new AirScoop(new BasicCar());
-		System.out.println("First Car" + carA.GetDescription());
+		ICar carA = new SuperCharger(new AirScoop (new BasicCar()));
+		System.out.println("Car1: " + carA.GetDescription());
+		System.out.println();
+
+		ICar carB = new AirScoop (new PaintColor(new BasicCar()));
+		System.out.println("Car2: " + carB.GetDescription());
+		System.out.println();
+
+		ICar carC = new SportWheels(new AirScoop (new BasicCar()));
+		System.out.println("Car3: " + carC.GetDescription());
+		System.out.println();
+
+		ICar carD = new SuperCharger(new SportWheels(new AirScoop (new BasicCar())));
+		System.out.println("Car4: " + carD.GetDescription());
+		System.out.println();
+
+		ICar carE = new PaintColor(new SuperCharger (new BasicCar()));
+		System.out.println("Car5: " + carE.GetDescription());
+		System.out.println();
+
+		ICar carF = new SuperCharger(new AirScoop (new SportWheels (new PaintColor(new BasicCar()))));
+		System.out.println("Car6: " + carF.GetDescription());
+		System.out.println();
 
 	}
 
