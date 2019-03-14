0efa58ba8f4c80b8be7da3a5ecc53354f4a119ed
Luke Helmcamp <lukehelmcamp@yahoo.com>
2019-03-13 23:22:44 -0500

    Update SportWheels.java

--- src/carcraft/SportWheels.java
+++ src/carcraft/SportWheels.java
@@ -1,10 +1,12 @@
 package carcraft;
 
+
 public class SportWheels extends CarOptions{
 	public SportWheels(ICar car) {
+		super(car);
 		System.out.println(" Adding sport wheels to vehicle...");
 	}
-	
+	//
 	@Override
 	public String GetDescription() {
 		return super.GetDescription() + " sport wheel";
