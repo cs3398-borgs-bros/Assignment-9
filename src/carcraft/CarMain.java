7d361dcc6fd4e480cb4659056e3eb56dc6405feb
dal79 <dal79@users.noreply.github.com>
2019-03-13 22:17:22 -0500

    Update CarMain.java
    Example of first car with a AirScoop

--- src/carcraft/CarMain.java
+++ src/carcraft/CarMain.java
@@ -1,11 +1,15 @@
-
 package carcraft;
 
 
 public class CarMain {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("Starting: Creating Cars using Decorator Pattern\n");
+
+		ICar carA = new AirScoop(new BasicCar());
+		System.out.println("First Car - " + carA.GetDescription());
+
 	}
 
 }
