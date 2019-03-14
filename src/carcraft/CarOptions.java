9322c6503f93bc7b3dfbef6f2c3bc946af748a43
Walter G <waltergarciaguirre@gmail.com>
2019-03-13 23:00:13 -0500

    Update CarOptions.java

--- src/carcraft/CarOptions.java
+++ src/carcraft/CarOptions.java
@@ -1,14 +1,17 @@
 package carcraft;
 
 public class CarOptions implements ICar {
+	
+	protected ICar car;
 
-	public CarOptions() {
+	public CarOptions(ICar c) {
+		this.car = c;
 		System.out.print("Car Add-ons: \n");
 	}
 	
 	@Override
 	public String GetDescription() {
-		return " with the following Options: " ;
+		return this.car.GetDescription();
 	}
 
 }
