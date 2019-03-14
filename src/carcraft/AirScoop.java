237f8389fca1b325790fd386751f3c93a99cce11
dal79 <dal79@users.noreply.github.com>
2019-03-13 21:49:10 -0500

    Create AirScoop.java

---
+++ src/carcraft/AirScoop.java
@@ -0,0 +1,13 @@
+package carcraft;
+
+public class AirScoop extends CarOptions {
+
+    public AirScoop(ICar car) {
+        System.out.println("Adding Air Scoop to vehicle..");
+    }
+
+    @Override
+    public String GetDescription() {
+		return super.GetDescription() + " AirScoop" ;
+	}
+}
