c9cde6227801370b147f667dd87c948cac6c231f
dal79 <dal79@users.noreply.github.com>
2019-03-13 23:07:18 -0500

    Update AirScoop.java

--- src/carcraft/AirScoop.java
+++ src/carcraft/AirScoop.java
@@ -3,6 +3,7 @@ package carcraft;
 public class AirScoop extends CarOptions {
 
     public AirScoop(ICar car) {
+        super(car);
         System.out.println(" Adding Air Scoop to vehicle..");
     }
 
